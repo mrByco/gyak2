f95b58cc7c278347d126ce42e53b8529bbcb07fc
Mátyás <matyi0918@gmail.com>
2021-10-06 07:26:15 +0200

    Algorithms

--- gyak5/main.c
+++ gyak5/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
@@ -27,7 +28,53 @@ int main()
         }
         numbersOk = 1;
     } while (!numbersOk);
+    printf("Start: %d, End: %d\n", start, end);
+
+    int array[10];
+    srand(time(NULL));
+    for (int i = 0; i < 10; i++){
+        array[i] = rand() % (end - start) + start;
+        printf("%d ", array[i]);
+    }
+
+    printf("\n");
+
+    // Count
+    printf("Type a number! Elements will be counted below that number!");
+    int upper_bound;
+    scanf("%d", &upper_bound);
+    int numbers_below = 0;
+    for (int i = 0; i < 10; i++){
+        if (array[i] < upper_bound){
+            numbers_below++;
+        }
+    }
+    printf("\nNumbers below: %d\n", numbers_below);
+
+    // Min
+    int min_result = array[0];
+    for (int i = 1; i < 10; i++){
+        if (array[i] < min_result){
+            min_result = array[i];
+        }
+    }
+    printf("\nMin: %d\n", min_result);
 
 
-    printf("Start: %d, End: %d", start, end);
+    // Array inverted
+    int array_inverted[10];
+    for (int i = 0; i < 10; i++){
+        array_inverted[9 - i] = array[i];
+    }
+
+    printf("Inverted array: ");
+    for (int i = 0; i < 10; i++){
+        printf("%d ", array_inverted[i]);
+    }
+
+    // SUM
+    // Multiply
+    // MIN
+    // MAX
+
 }
