d378eb538d7529c07c6e43e2ee7f6777f9d9b301
Byc <matiy0918@gmail.com>
2021-11-27 11:55:57 +0100

    5x^8

--- sz_argitektura/derivator/derivate.sh
+++ sz_argitektura/derivator/derivate.sh
@@ -11,10 +11,12 @@ Help() {
 }
 
 DerivateSingle() {
-  # Executes a single non composite subderivation
+  # Executes a single non composite subderivation oiasjdolkajsdlk
+
+  
 
   # x^5, x^5342, x^-234234
-  if [[ $1 =~ ^([a-z]\^-*[0-9]+)$ ]]; then
+  if [[ $1 =~ ^([a-z]\^-?[0-9]*)$ ]]; then
       exponent=$(echo $1 | grep -oP "\-*[0-9]+")
       if [[ $exponent == 2 ]]; then
         echo "2x"
@@ -22,6 +24,21 @@ DerivateSingle() {
         # shellcheck disable=SC2140
         echo "$exponent"x^"$(("$exponent"-1))"
       fi
+  # 54x^5, -231x^5342, 4x^-234234
+  elif [[ $1 =~ ^(-?[0-9]+)?[a-z]\^-?[0-9]*$ ]]; then
+        prefix=$(echo $1 | grep -oP "(-)?[0-9]*x" | grep -oP "(-)?[0-9]*")
+        exponent=$(echo $1 | grep -oP "\^(-)?[0-9]*" | grep -oP "(-)?[0-9]*")
+        if [[ $exponent == 2 ]]; then
+          echo "$(($prefix*$exponent))"x
+        else
+        # shellcheck disable=SC2140
+          echo "$(($prefix*$exponent))"x^"$(("$exponent"-1))"
+        fi
+  # s
+  elif [[ $1 =~ ^(-?[0-9]+)?[a-z]\^-?[0-9]*$ ]]; then
+        prefix=$(echo $1 | grep -oP "-?[0-9]*x")
+        exponent=$(echo $1 | grep -oP "\^-*[0-9]*" | grep -oP "\-*[0-9]*")
+        echo "$($prefix * $exponent)"x^"$(("$exponent"-1))"
   else
       echo "Not implemented"
   fi
@@ -54,4 +71,3 @@ done
 
 result=$(DerivationLoop $1)
 echo "Result: " $result
-read
